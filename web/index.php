687948faf1f2ff8ee2fb5cb7dc3b6e254a110b04
BEHeder <36495935+BEHeder@users.noreply.github.com>
2020-01-18 17:56:49 -0700

    adding content to intro page

--- web/index.php
+++ web/index.php
@@ -1,5 +1,5 @@
 <?php
-  echo "<a href='./intro.html'>Introduction Page</a>";
+  echo "<a href='./intro.html'>Introduction Page</a></br>";
   echo "<a href='./assignments.html'>Assignments Page</a>";
 
 ?>
