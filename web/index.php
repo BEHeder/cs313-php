94b93d5a53344ddd582240cfb1f97e94a21cf5f8
BEHeder <36495935+BEHeder@users.noreply.github.com>
2020-01-18 20:14:10 -0700

    adding css file and timestamp

--- web/index.php
+++ web/index.php
@@ -1,5 +1,13 @@
-<?php
-  echo "<a href='./intro.html'>Introduction Page</a></br>";
-  echo "<a href='./assignments.html'>Assignments Page</a>";
-
-?>
+<!DOCTYPE html>
+<head>
+  <title>B. Heder's Home Page</title>
+  <link rel="stylesheet" type="text/css" href="./mystyle.css">
+</head>
+<body>
+  <?php
+    echo "<a href='./intro.html'>Introduction Page</a></br>";
+    echo "<a href='./assignments.html'>Assignments Page</a>";
+    date_default_timezone_set("America/Boise");
+    date("h:i:s a, l, m/d/Y");
+  ?>
+</body>
