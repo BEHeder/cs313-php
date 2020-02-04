879d23cdd760a43d8644c11561dda2970bcd5510
BEHeder <36495935+BEHeder@users.noreply.github.com>
2020-02-04 16:47:38 -0700

    end of class

--- web/week05/team05BH.php
+++ web/week05/team05BH.php
@@ -1,6 +1,13 @@
 <?php
     require "dbConnect.php";
-    echo "<h1>Scripture Resources</h1></br>";
+    echo "
+        <h1>Scripture Resources</h1></br>
+        <form action=" . htmlspecialchars($_SERVER["PHP_SELF"]) . " method='post'>
+            Book:
+            <input type='text' name='book' id='book'><br>
+            <input type='submit' name='Submit' value='Submit'>
+        </form>
+    ";
     $db = get_db(); //if the function doesn't return anything, then this could throw a 500 error
 
     $scriptures = $db->prepare("SELECT * FROM scriptures"); //prepare() helps ensure you have
