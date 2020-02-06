e93a447c9c4ce6f0aa5a0e02ba921ee1341c2e68
BEHeder <36495935+BEHeder@users.noreply.github.com>
2020-02-06 16:32:18 -0700

    w5 thursday class

---
+++ web/class/w5ThursdayClassActivity/index.php
@@ -0,0 +1,16 @@
+<?php 
+    require "dbConnect.php";
+    $db = get_db();
+
+    $events = $db->prepare("SELECT * FROM w5_EVENT");
+    $events->execute();
+
+    while ($row = $events->fetch(PDO::FETCH_ASSOC))
+    {
+        $name = $row["name"];
+        $image = $row["image"];
+
+        echo "<p>$name <img src='$image'></p>";
+
+    }
+?>
