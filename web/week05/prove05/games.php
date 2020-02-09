0763072fa70f32885021e94146a1054df5040c9f
BEHeder <36495935+BEHeder@users.noreply.github.com>
2020-02-08 18:11:56 -0700

    eh

--- web/week05/prove05/games.php
+++ web/week05/prove05/games.php
@@ -5,7 +5,7 @@
     $games = $db->prepare("SELECT * FROM game");
     $games->execute();
 
-    while ($row = $events->fetch(PDO::FETCH_ASSOC))
+    while ($row = $games->fetch(PDO::FETCH_ASSOC))
     {
         $name = $row["name"];
         echo "<p>$name</p>";
