188d1692ee878226bc590fc584258ce821157e30
BEHeder <36495935+BEHeder@users.noreply.github.com>
2020-02-08 21:12:26 -0700

    list games

--- web/week05/prove05/games.php
+++ web/week05/prove05/games.php
@@ -1,13 +1,27 @@
-<?php 
-    require "../../shared/dbConnect.php";
-    $db = get_db();
-
-    $games = $db->prepare("SELECT * FROM game");
-    $games->execute();
-
-    while ($row = $games->fetch(PDO::FETCH_ASSOC))
-    {
-        $name = $row["name"];
-        echo "<p>$name</p>";
-    }
+<?php
+    session_start();
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+    <body>
+        <h1>List of Games</h1>
+        <ul>
+            <?php 
+                require "../../shared/dbConnect.php";
+                $db = get_db();
+
+                $games = $db->prepare("SELECT * FROM game");
+                $games->execute();
+
+                while ($row = $games->fetch(PDO::FETCH_ASSOC))
+                {
+                    $name = $row["name"];
+                    echo "<li>$name</li>";
+                }
+            ?>
+        </ul>
+    </body>
+</html>
