68bf07a8213e2d1164801acd5c1e961ad9a4754c
BEHeder <36495935+BEHeder@users.noreply.github.com>
2020-02-11 16:46:22 -0700

    team06

--- web/week06/team06/insert.php
+++ web/week06/team06/insert.php
@@ -1,3 +1,10 @@
 <?php
     require "../../shared/dbConnect.php";
+    $db = get_db();
+
+    $book = $_POST['book'];
+    // $book = $_POST['book'];
+    // $book = $_POST['book'];
+    // $book = $_POST['book'];
+    
 ?>
